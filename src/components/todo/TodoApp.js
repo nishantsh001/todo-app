789ac237befcfd91c41a7fe68b41d4cbc384f377
nishantsh001 <nishant.sh1999@gmail.com>
2024-12-25 11:53:12 +0530

    second

--- src/components/todo/TodoApp.js
+++ src/components/todo/TodoApp.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import './Todo.css';
+import { BrowserRouter, Route, Routes, useNavigate, useParams } from 'react-router-dom';
 
 export default function TodoApp() {
   return (
     <div className='TodoApp'>
-      Todo Management Application
-      <LoginComponent />
-      {/* <WelcomeComponent /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginComponent />} />
+          <Route path="/login" element={<LoginComponent />} />
+          <Route path="/welcome/:username" element={<WelcomeComponent />} />
+          <Route path="/todos" element={<ListTodosComponent />} />
+          <Route path="*" element={<ErrorComponent />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
@@ -16,6 +23,8 @@ function LoginComponent() {
   const [password, setPassword] = useState('');
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleUsernameChange = (e) => {
     console.log(e.target.value);
@@ -32,6 +41,7 @@ function LoginComponent() {
       console.log('Login Successful');
       setShowSuccessMessage(true);
       setShowErrorMessage(false);
+      navigate(`/welcome/${username}`);
     } else {
       console.log('Login Failed');
       setShowSuccessMessage(false);
@@ -39,27 +49,25 @@ function LoginComponent() {
     }
   }
 
-  function SuccessMessageComponent() {
-    if (showSuccessMessage)
-      return <div className='successMessage'>Authenticated Successfully</div>;
-    return null;
-  }
+  // function SuccessMessageComponent() {
+  //   if (showSuccessMessage)
+  //      return <div className='successMessage'>Authenticated Successfully</div>
+  //   return
+  //   return null;
+  // }
 
-  function ErrorMessageComponent() {
-    if (showErrorMessage)
-      return (
-        <div className='errorMessage'>
-          Authentication Failed. Please check your credentials.
-        </div>
-      );
-    return null;
-  }
+  // function ErrorMessageComponent() {
+  //   if (showErrorMessage)
+  //     return <div className='errorMessage'>Authentication Failed. Please check your credentials.</div>;
+  //   return null;
+  // }
 
   return (
     <div className='Login'>
-      <SuccessMessageComponent />
-      <ErrorMessageComponent />
+      {showSuccessMessage && <div className='successMessage'>Authenticated Successfully</div>}
+      {showErrorMessage && <div className='errorMessage'>Authentication Failed. Please check your credentials.</div>}
       <div className='Login'>
+        <h1>Time to study!</h1>
         <div>
           <label>User Name</label>
           <input
@@ -89,5 +97,64 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
-  return <div className='Welcome'>Welcome Component</div>;
+  // const params =  useParams();
+  // console.log(params.username);
+  const { username } = useParams();
+  console.log(username);
+  return (
+    <div className='Welcome'>
+      <h1>Welcome {username}</h1>
+      Welcome Component
+    </div>
+  );
+}
+
+function ErrorComponent() {
+  return (
+    <div className='ErrorComponent'>
+      <h1>we are working really hard!</h1>
+      <div>
+        Apologies for 404. reach out our team.....
+      </div>
+    </div>
+  );
+}
+
+
+function ListTodosComponent() {
+
+  const todos = [
+    { id: 1, description: 'Learn AWS' },
+    { id: 2, description: 'Learn Full Stack' },
+    { id: 3, description: 'Learn Devops' },
+  ]
+
+  return (
+    <div className='ListTodosComponent'>
+      <h1>List Todos</h1>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <td>id</td>
+              <td>description</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              todos.map(todo => (
+                <tr key={todo.id}>
+                  <td>{todo.id}</td>
+                  <td>{todo.description}</td>
+                </tr>
+              )
+              )
+            }
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+  );
 }
